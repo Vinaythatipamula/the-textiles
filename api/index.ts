8799ced242b5c1e4c361fd023ad5b67452ec43f6
Vinaythatipamula <vinaythatipamula7@gmail.com>
2025-07-21 15:34:17 +0530

    aws-lambda issue is fixed

--- api/index.ts
+++ api/index.ts
@@ -1,20 +1,24 @@
-import { Handler } from 'aws-lambda';
+import { APIGatewayProxyEvent, Context, Callback } from 'aws-lambda';
 import serverlessExpress from '@vendia/serverless-express';
 import { AppModule } from '../src/app.module';
 import { NestFactory } from '@nestjs/core';
 
-let cachedServer: Handler;
+let cachedServer: any;
 
-async function bootstrap(): Promise<Handler> {
+async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.init();
   const expressApp = app.getHttpAdapter().getInstance();
   return serverlessExpress({ app: expressApp });
 }
 
-export const handler: Handler = async (event, context) => {
+export const handler = async (
+  event: APIGatewayProxyEvent,
+  context: Context,
+  callback: Callback
+) => {
   if (!cachedServer) {
     cachedServer = await bootstrap();
   }
-  return cachedServer(event, context);
+  return cachedServer(event, context, callback);
 };
